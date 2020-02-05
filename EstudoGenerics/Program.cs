939c663a61b83260bf79a98c8e3cdd8ac6d25888
Ewerton Marques <marquescpv@gmail.com>
2020-02-05 10:48:28 -0300

    Estudo Generics

---
+++ EstudoGenerics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa
+            {
+                Id = 1,
+                Nome = "Teste"
+
+            };
+            Animal animal = new Animal
+            {
+                Id = 1,
+                Especie = "cachorro"
+            };
+            RepositorioGenerico<Pessoa> Pessoas = new RepositorioGenerico<Pessoa>();
+            RepositorioGenerico<Animal> Animais = new RepositorioGenerico<Animal>();
+
+            Pessoas.Insert(pessoa);
+            Animais.Insert(animal);
+
+            foreach(Pessoa p in Pessoas.Get())
+            {
+                Console.WriteLine(p.Nome);
+            }
+
+            foreach(Animal a in Animais.Get())
+            {
+                Console.WriteLine(a.Especie);
+            }
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ EstudoGenerics/RepositorioGenerico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstudoGenerics
+{
+    public class RepositorioGenerico<T>
+    {
+        private List<T> list;
+
+        public RepositorioGenerico()
+        {
+            list = new List<T>();
+        }
+
+        public List<T> Get()
+        {
+            return list;
+        }
+
+        public void Insert(T objeto)
+        {
+            list.Add(objeto);
+        }
+    }
+}
